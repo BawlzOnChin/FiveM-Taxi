76bddf25540e1eae8bf6eb52d7ce4fc7af796acc
Glenn Hermans <glhermans@gmail.com>
2017-03-11 18:16:34 +0100

    Fixed #1

--- taxi_sv.lua
+++ taxi_sv.lua
@@ -6,7 +6,7 @@ TriggerEvent('es:getPlayerFromId', source, function(user)
   print('Player current cash ' ..user.money)
   print('Player permission ' ..user.permission_level)
 
-  total = tonumber(user.money) + 500;
+  total = tonumber(user.money) + math.random(100, 500);
   print('Players new cash amount ' ..total)
   -- update player money amount
  TriggerEvent("es:setPlayerData", source, "money", total, function(response, success)
